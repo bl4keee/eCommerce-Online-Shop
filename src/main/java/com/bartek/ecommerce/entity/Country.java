daf1041342d85f027000419da29f8dd72d954cda
Bartek <bl4kee@wp.pl>
2020-11-20 19:09:06 +0100

    Developed Repositories for Country and State, added JsonIgnore to Country Entity to ignore the states and show countries only

--- src/main/java/com/bartek/ecommerce/entity/Country.java
+++ src/main/java/com/bartek/ecommerce/entity/Country.java
@@ -1,5 +1,6 @@
 package com.bartek.ecommerce.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,5 +25,6 @@ public class Country {
     private String name;
 
     @OneToMany(mappedBy = "country")
+    @JsonIgnore //will ignore the states and show only countries
     private List<State> states;
 }
