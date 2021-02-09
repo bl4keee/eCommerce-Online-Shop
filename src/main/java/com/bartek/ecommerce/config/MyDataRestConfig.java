302b623857bb6b260ca4aec66c08fb076063a597
Bartek <bl4kee@wp.pl>
2021-02-09 13:44:38 +0100

    Configured CORS mapping

--- src/main/java/com/bartek/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/bartek/ecommerce/config/MyDataRestConfig.java
@@ -5,6 +5,7 @@ import com.bartek.ecommerce.entity.Product;
 import com.bartek.ecommerce.entity.ProductCategory;
 import com.bartek.ecommerce.entity.State;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -19,6 +20,9 @@ import java.util.Set;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Value("${allowed.origins}")
+    private String[] theAllowedOrigins;
 
     private EntityManager entityManager;
 
@@ -37,7 +41,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         disableHttpMethods(State.class, config, theUnsupportedActions);
 
         exposeIds(config);
-        cors.addMapping("/api/**").allowedOrigins("http://localhost:4200");
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
     }
 
     private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
