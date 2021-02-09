46d7ebd2d1c69020eb94c6a14cf20d8e43858eef
Bartek <bl4kee@wp.pl>
2021-02-09 13:49:21 +0100

    Configured Cors mapping for Checkout Rest Controller

--- src/main/java/com/bartek/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/bartek/ecommerce/controller/CheckoutController.java
@@ -5,7 +5,6 @@ import com.bartek.ecommerce.dto.PurchaseResponse;
 import com.bartek.ecommerce.service.CheckoutService;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("api/checkout")
 public class CheckoutController {
